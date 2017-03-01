6218e10a3b4586971faf8d2197eed3d479ee02da
adrianjrandall <adrianj.randall@gmail.com>
2017-03-01 06:48:16 -0500

    added file to run MC sim on all possible hands and output a json file with win probabilites and ranks

---
+++ all_hand_sims.py
@@ -0,0 +1,27 @@
+import json
+from hand_generator import simulate_hand
+from numpy import mean
+
+cards = '23456789TJQKA'
+suits = 'AHDC'
+deck = [x+y for x in cards for y in suits]
+
+# generate a json file containing results from simulation on all cards
+
+# first generate all possible two card hands
+
+all_poss_hands = [tuple(sorted((card, other_card))) for card in deck for other_card in deck[:deck.index(card)]+deck[deck.index(card)+1:]]
+# remove duplicates
+all_poss_hands = list(set(all_poss_hands))
+
+# generate outputs from hand_generator and export to json file
+output_dict = {}
+with open('hand_win_prob_and_ranks.json', 'w') as json_out:
+	for hand in all_poss_hands:
+		print all_poss_hands.index(hand)
+		output, rankings = simulate_hand(' '.join(hand), villains=4, sims=1000)
+		output_dict[' '.join(hand)] = {
+			'win_prob': mean(output),
+			'rankings': mean(rankings)
+		}
+	json.dump(output_dict, json_out)
