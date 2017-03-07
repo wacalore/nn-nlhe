9474bc994b87784d985205da079e42c3d97a42cb
Anthony Calore <anthony.calore@gmail.com>
2017-03-06 23:36:44 -0500

    Implemented range generator for 4 different input patterns (suited, offsuit, any, and + range)

--- rangegenerator.py
+++ rangegenerator.py
@@ -13,6 +13,8 @@ import pyparsing as pp
 import itertools
 
 
+rankvalues = dict((r, i)
+                  for i, r in enumerate('..23456789TJQKA'))
 
 class RangeBuilder:
 
@@ -32,13 +34,15 @@ class RangeBuilder:
 def parse_range(userinput):
 
     hand_range = []
-    tokens = userinput.split()
+    if len(userinput) > 1:
+        tokens = userinput.split()
     deck = Deck()
 
     for token in tokens:
 
         suited = False
         offsuit = False
+        ascending = False
 
         if 's' in token:
             suited = True
@@ -46,20 +50,34 @@ def parse_range(userinput):
         elif 'o' in token:
             offsuit = True
             token = token[0:-1]
+        elif '+' in token:
+            ascending = True
+            token = token[0:-1]
+            asc_rank = token[-1]
         else:
             pass
 
         cards = deck.slice_deck(token)
+        rank = list(token)
+        card_set1 = deck.slice_deck(rank[0])
+        card_set2 = deck.slice_deck(rank[1])
+        all_combos = list(itertools.product(card_set1, card_set2))
 
         if suited:
-            rank = list(token)
-            for suit in deck.suits:
-                hand_range.append([rank[0]+suit+rank[1]+suit])
+            suited_combos = [(x, y) for x, y in all_combos if x[1] == y[1]]
+            hand_range += suited_combos
         elif offsuit:
-            # TODO deal with offsuit
-            pass
+            offsuit_combos = [(x, y) for x, y in all_combos if x[1] != y[1]]
+            hand_range += offsuit_combos
+        elif ascending:
+            hand_range += all_combos
+            ascending_ranks = [x for x in rankvalues if rankvalues[asc_rank] > rankvalues[x] < rankvalues[rank[0]]]
+            for card in ascending_ranks:
+                asc_cards = deck.slice_deck(card)
+                hand_range += list(itertools.product(card_set1, asc_cards))
         else:
-            hand_range.append(list(itertools.combinations(cards, 2)))
+            hand_range += all_combos
+
 
     '''
     lower_bound = pp.Word(pp.nums).setResultsName('Lower')
